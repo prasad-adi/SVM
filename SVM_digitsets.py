0300ebbcdac618b5e6e71e341f3f90fecc5acb26
prasad-adi <49210266+prasad-adi@users.noreply.github.com>
2019-04-15 12:33:42 -0400

    Update SVM_digitsets.py

--- SVM_digitsets.py
+++ SVM_digitsets.py
@@ -10,8 +10,8 @@ class SVM_Digits:
     def read_data(self):
         with zipfile.ZipFile("./features50.txt.zip","r") as zip_ref:
             zip_ref.extractall("./")
-        X = np.loadtxt("./features50.txt")
-        Y = np.loadtxt("./labels50.txt")
+        X = np.loadtxt("./features30.txt")
+        Y = np.loadtxt("./labels30.txt")
         data = np.c_[X, Y]
         np.random.shuffle(data)
         train_size = int(data.shape[0] * 0.8)
